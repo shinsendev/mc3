50504dd5ea521d1d910cec12fbc5adeb49d43d3d
gdarquie <gaetan.darquie@gmail.com>
2020-05-14 21:13:05 +0200

    MC-6: upgrade number model

--- src/Entity/Number.php
+++ src/Entity/Number.php
@@ -21,6 +21,26 @@ class Number extends AbstractTarget
      */
     private $film;
 
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $beginTc;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $endTc;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $shots;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $quotation;
+
     public function getTitle(): ?string
     {
         return $this->title;
@@ -41,6 +61,54 @@ class Number extends AbstractTarget
     public function setFilm(?film $film): self
     {
         $this->film = $film;
+
+        return $this;
+    }
+
+    public function getBeginTc(): ?int
+    {
+        return $this->beginTc;
+    }
+
+    public function setBeginTc(?int $beginTc): self
+    {
+        $this->beginTc = $beginTc;
+
+        return $this;
+    }
+
+    public function getEndTc(): ?int
+    {
+        return $this->endTc;
+    }
+
+    public function setEndTc(?int $endTc): self
+    {
+        $this->endTc = $endTc;
+
+        return $this;
+    }
+
+    public function getShots(): ?int
+    {
+        return $this->shots;
+    }
+
+    public function setShots(?int $shots): self
+    {
+        $this->shots = $shots;
+
+        return $this;
+    }
+
+    public function getQuotation(): ?string
+    {
+        return $this->quotation;
+    }
+
+    public function setQuotation(?string $quotation): self
+    {
+        $this->quotation = $quotation;
 
         return $this;
     }
