be88808cc0ecb12b86f14bf6b11cc13544e6e405
gdarquie <gaetan.darquie@gmail.com>
2020-05-15 17:19:04 +0200

    MC-6: upgrade person model

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -10,13 +10,88 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Person extends AbstractTarget
 {
-    private $groupname;
-
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
     private $firstname;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
     private $lastname;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $groupname;
+
+    /**
+     * @ORM\Column(type="string", length=5, nullable=true)
+     */
     private $gender;
 
-    private $imdb;
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $viaf;
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getGroupname(): ?string
+    {
+        return $this->groupname;
+    }
+
+    public function setGroupname(string $groupname): self
+    {
+        $this->groupname = $groupname;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getViaf(): ?string
+    {
+        return $this->viaf;
+    }
+
+    public function setViaf(?string $viaf): self
+    {
+        $this->viaf = $viaf;
+
+        return $this;
+    }
 }
