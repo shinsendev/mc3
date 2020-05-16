685ea992d95dbd318a7527f07db9ec99fca234c4
gdarquie <gaetan.darquie@gmail.com>
2020-05-16 12:21:25 +0200

    MC-6: upgrade studio

--- src/Entity/Studio.php
+++ src/Entity/Studio.php
@@ -10,4 +10,20 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Studio extends AbstractTarget
 {
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
