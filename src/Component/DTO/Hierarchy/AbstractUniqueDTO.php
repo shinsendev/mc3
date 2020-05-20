4e9b91d469092989ad06f3bab4f204f2238f6621
gdarquie <gaetan.darquie@gmail.com>
2020-05-20 02:33:09 +0200

    MC-12: get SongDTO

---
+++ src/Component/DTO/Hierarchy/AbstractUniqueDTO.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Component\DTO\Hierarchy;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+
+/**
+ * Class AbstractUniqueDTO
+ * @package App\Component\DTO\Hierarchy
+ */
+class AbstractUniqueDTO
+{
+    /**
+     * @ApiProperty(identifier=true)
+     */
+    protected $uuid;
+
+    /**
+     * @return mixed
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @param mixed $uuid
+     */
+    public function setUuid($uuid): void
+    {
+        $this->uuid = $uuid;
+    }
+}
--- src/Component/DTO/Payload/SongPayloadDTO.php
+++ src/Component/DTO/Payload/SongPayloadDTO.php
@@ -2,11 +2,10 @@
 
 declare(strict_types=1);
 
-
 namespace App\Component\DTO\Payload;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use App\Component\DTO\Composition\UniqueDTOTrait;
+use App\Component\DTO\Hierarchy\AbstractUniqueDTO;
 
 /**
  * Class NarrativeDTO
@@ -15,7 +14,72 @@ use App\Component\DTO\Composition\UniqueDTOTrait;
  *     shortName="song"
  * )
  */
-class SongPayloadDTO
+class SongPayloadDTO extends AbstractUniqueDTO
 {
-    use UniqueDTOTrait;
+    /** @var string */
+    private $title;
+
+    /** @var integer */
+    private $year;
+
+    /** @var string */
+    private $externalId;
+
+    public function hydrate(array $data)
+    {
+        $song = $data['song'];
+        $this->setTitle($song->getTitle());
+        $this->setYear($song->getYear());
+        $this->setExternalId($song->getExternalId());
+        $this->setUuid($song->getUuid());
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param int $year
+     */
+    public function setYear(?int $year): void
+    {
+        $this->year = $year;
+    }
+
+    /**
+     * @return string
+     */
+    public function getExternalId(): ?string
+    {
+        return $this->externalId;
+    }
+
+    /**
+     * @param string $externalId
+     */
+    public function setExternalId(?string $externalId): void
+    {
+        $this->externalId = $externalId;
+    }
+
 }
