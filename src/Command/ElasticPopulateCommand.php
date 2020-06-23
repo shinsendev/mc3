830147557b1764222b7deca81e9686ff8df3841f
gdarquie <gaetan.darquie@gmail.com>
2020-06-23 23:51:07 +0200

    MC-39: WIP first indexation of all movies'

---
+++ src/Command/ElasticPopulateCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Command;
+
+use App\Component\Elastic\ElasticConnection;
+use App\Component\Elastic\Indexer;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ElasticPopulateCommand extends Command
+{
+    protected static $defaultName = 'elastic:populate';
+
+    /** @var EntityManagerInterface */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em, string $name = null)
+    {
+        parent::__construct($name);
+        $this->em = $em;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        Indexer::populate($this->em);
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return 0;
+    }
+}
