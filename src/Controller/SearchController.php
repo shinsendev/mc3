55ca018d6870842070cbe2a9fea90ccd360c7b6c
gdarquie <gaetan.darquie@gmail.com>
2020-06-24 16:28:38 +0200

    Post route for search able to handle params

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,31 +5,22 @@ namespace App\Controller;
 use App\Component\Elastic\ElasticConnection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class SearchController
+ * @package App\Controller
+ */
 class SearchController extends AbstractController
 {
     /**
-     * @Route("/search", name="search")
+     * @Route("/search", methods={"POST"})
      */
-    public function getResults(string $type = null)
+    public function search(Request $request)
     {
-        // todo: create a post route, get request from parameters
         $client = ElasticConnection::connect();
-
-        $params = [
-            'index' => 'mc2',
-            'body'  => [
-                'query' => [
-                    'match' => [
-                        'title' => 'West'
-                    ]
-                ]
-            ]
-        ];
-
-
-        $results = $client->search($params);
+        $results = $client->search(json_decode($request->getContent(), true));
 
         return new JsonResponse($results);
     }
