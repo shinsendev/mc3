e8c10a78784f2fa065c103f17d53d7b894fc1801
gdarquie <gaetan.darquie@gmail.com>
2020-06-25 12:09:29 +0200

    Add NestedPersonHydrator strategy and debug homepage

--- src/Component/Hydrator/Strategy/NestedPersonPayloadHydrator.php
+++ src/Component/Hydrator/Strategy/NestedPersonPayloadHydrator.php
@@ -2,17 +2,27 @@
 
 declare(strict_types=1);
 
-
 namespace App\Component\Hydrator\Strategy;
 
-
 use App\Component\DTO\Definition\DTOInterface;
+use App\Component\DTO\Payload\PersonPayloadDTO;
+use App\Component\Factory\DTOFactory;
 use App\Component\Hydrator\Description\HydratorDTOInterface;
+use App\Component\Model\ModelConstants;
 use App\Entity\Person;
 use Doctrine\ORM\EntityManagerInterface;
 
 class NestedPersonPayloadHydrator implements HydratorDTOInterface
 {
+    const GROUP_TYPE = 'group';
+    const PERSON_TYPE = 'person';
+
+    /**
+     * @param PersonPayloadDTO $dto
+     * @param array $data
+     * @param EntityManagerInterface $em
+     * @return DTOInterface
+     */
     public static function hydrate(DTOInterface $dto, array $data, EntityManagerInterface $em):DTOInterface
     {
         /** @var Person $person */
