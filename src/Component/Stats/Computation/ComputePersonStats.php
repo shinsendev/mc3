2d766db1d7ca8a79f96d55aa7293788343965c6a
gdarquie <gaetan.darquie@gmail.com>
2020-07-02 19:08:00 +0200

    Add normalizer for stats

--- src/Component/Stats/Computation/ComputePersonStats.php
+++ src/Component/Stats/Computation/ComputePersonStats.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace App\Component\Stats\Computation;
 
 
+use App\Component\DTO\Stats\Person\NestedFilmsInPersonStatsDTO;
 use App\Entity\Person;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -19,5 +20,16 @@ class ComputePersonStats
     public static function computeAverageShotLength(string $personUuid, EntityManagerInterface $em):int
     {
         return intval(100*round($em->getRepository(Person::class)->computeAverageShotLength($personUuid),2));
+    }
+
+    /**
+     * @return NestedFilmsInPersonStatsDTO
+     */
+    public static function generateFilmStats()
+    {
+        $film = new NestedFilmsInPersonStatsDTO();
+        $film = [];
+
+        return $film;
     }
 }
--- src/Component/Stats/Strategy/PersonStatsStrategy.php
+++ src/Component/Stats/Strategy/PersonStatsStrategy.php
@@ -9,6 +9,9 @@ use App\Component\Model\ModelConstants;
 use App\Component\Stats\Computation\ComputePersonStats;
 use App\Entity\Statistic;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Class PersonStatsStrategy
@@ -25,15 +28,21 @@ class PersonStatsStrategy
     public static function saveStats(string $personUuid, EntityManagerInterface $em):void
     {
         // if stat already exists, it's an update, if not we create a new stat
-        if (!$stat = $em->getRepository(Statistic::class)->findBy(['targetUuid' => $personUuid, 'key' => self::PERSON_STATS_KEY])) {
+        if (!$stat = $em->getRepository(Statistic::class)->findOneBy(['targetUuid' => $personUuid, 'key' => self::PERSON_STATS_KEY])) {
             $stat = new Statistic();
             $stat->setKey(self::PERSON_STATS_KEY);
             $stat->setTargetUuid($personUuid);
             $stat->setModel(ModelConstants::PERSON_MODEL);
         }
 
-        $computeStats = self::computeStats($personUuid, $em);
-        $stat->setValue($computeStats);
+        /** @var PersonStatsDTO $personStatsDTO */
+        $personStatsDTO = self::computeStats($personUuid, $em);
+
+        // convert PersonStatsDTO into array
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers);
+        $stat->setValue($serializer->normalize($personStatsDTO));
+
         $em->persist($stat);
         $em->flush();
     }
@@ -43,15 +52,14 @@ class PersonStatsStrategy
      * @param EntityManagerInterface $em
      * @return PersonStatsDTO[]|array
      */
-    private function computeStats(string $personUuid, EntityManagerInterface $em): array
+    private function computeStats(string $personUuid, EntityManagerInterface $em):PersonStatsDTO
     {
         $personStats = new PersonStatsDTO(); // to serialise in json array
         $personStats->setAverageShotLength(ComputePersonStats::computeAverageShotLength($personUuid, $em));
-        dd($personStats);
 
-        $films = [];
-        $stat->setFilms([$films]);
+        $films = ComputePersonStats::generateFilmStats();
+        $personStats->setFilms([$films]);
 
-        return [$stat];
+        return $personStats;
     }
 }
