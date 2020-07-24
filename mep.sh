4f734939aa6be0eb682ea83e3111d11633e76cc4
gdarquie <gaetan.darquie@gmail.com>
2020-07-24 15:03:08 +0200

    Add permissions for stats

--- mep.sh
+++ mep.sh
@@ -2,6 +2,6 @@ sudo git pull upstream master
 sudo composer install
 php bin/console do:mi:mi --no-interaction
 sudo php bin/console cache:clear
-php bin/console stats:person:update
-php bin/console elastic:populate
-php bin/console algolia:populate
+sudo php bin/console stats:person:update
+sudo php bin/console elastic:populate
+sudo php bin/console algolia:populate
