2e2fe1733ae851d7c20330485b14146f1c001dea
gdarquie <gaetan.darquie@gmail.com>
2020-07-31 15:39:11 +0200

    Add attributes for number nested in filmDTO for film timelin

--- src/Component/DTO/Nested/NumberNestedInFilmDTO.php
+++ src/Component/DTO/Nested/NumberNestedInFilmDTO.php
@@ -20,6 +20,14 @@ class NumberNestedInFilmDTO extends AbstractUniqueDTO
     private int $length = 0;
     private array $performers = [];
 
+    // categories for timeline visualisation
+    private array $performance = [];
+    private array $structure = [];
+    private array $completeness = [];
+    private array $diegetic = [];
+    private array $cast = [];
+    private array $source = []; // multiple choice
+
     /**
      * @return int
      */
@@ -114,6 +122,102 @@ class NumberNestedInFilmDTO extends AbstractUniqueDTO
     public function setPerformers(array $performers): void
     {
         $this->performers = $performers;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPerformance(): array
+    {
+        return $this->performance;
+    }
+
+    /**
+     * @param array $performance
+     */
+    public function setPerformance(array $performance): void
+    {
+        $this->performance = $performance;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStructure(): array
+    {
+        return $this->structure;
+    }
+
+    /**
+     * @param array $structure
+     */
+    public function setStructure(array $structure): void
+    {
+        $this->structure = $structure;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCompleteness(): array
+    {
+        return $this->completeness;
+    }
+
+    /**
+     * @param array $completeness
+     */
+    public function setCompleteness(array $completeness): void
+    {
+        $this->completeness = $completeness;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDiegetic(): array
+    {
+        return $this->diegetic;
+    }
+
+    /**
+     * @param array $diegetic
+     */
+    public function setDiegetic(array $diegetic): void
+    {
+        $this->diegetic = $diegetic;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSource(): array
+    {
+        return $this->source;
+    }
+
+    /**
+     * @param array $source
+     */
+    public function setSource(array $source): void
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCast(): array
+    {
+        return $this->cast;
+    }
+
+    /**
+     * @param array $cast
+     */
+    public function setCast(array $cast): void
+    {
+        $this->cast = $cast;
     }
 
 }
