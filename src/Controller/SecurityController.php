fcc8547181788ff3ce2954cc07b36e46e75d9c5d
gdarquie <gaetan.darquie@gmail.com>
2020-08-06 23:25:51 +0200

    First tests for authentication with firebase

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,14 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Auth;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login", methods={"POST"})
      */
-    public function index()
+    public function login()
     {
+        $factory = (new Factory)->withServiceAccount('../keys/firebase.json');
+        $auth = $factory->createAuth();
+
+        $uid = 'some-uid';
+
+        $customToken = $auth->createCustomToken($uid);
+        dd($customToken);
+
         return $this->json([
                 'user' => $this->getUser() ? $this->getUser()->getId() : null]
         );
