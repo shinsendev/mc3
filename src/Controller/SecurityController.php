c113f041e3a3d7436eecdf411228db4d3f4dbdf1
gdarquie <gaetan.darquie@gmail.com>
2020-08-07 21:31:08 +0200

    Prepare Auhtentication system with firebase

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,47 +2,55 @@
 
 namespace App\Controller;
 
+use App\Component\Authentication\Authentication;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Kreait\Firebase\Factory;
-use Kreait\Firebase\Auth;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login", methods={"POST"})
      */
-    public function login()
+    public function login(Request $request)
+    {
+        $auth = Authentication::createFirebaseAuth();
+
+        $content = json_decode($request->getContent());
+        $email = $content->email;
+        $password = $content->password;
+
+        //return a token front can keep
+        return $this->json(Authentication::checkPassword($auth, $email, $password), 200);
+    }
+
+    private function checkPassword($auth, string $email, string $password)
+    {
+        return ($auth->signInWithEmailAndPassword($email, $password))->data();
+    }
+
+    /**
+     * @Route("/refresh_token", name="checkToken", methods={"POST"})
+     */
+    public function refreshToken(Request $request)
+    {
+        $auth = Authentication::createFirebaseAuth();
+        $refreshToken = "";
+
+        $signInResult = $auth->signInWithRefreshToken($refreshToken);
+        dd($signInResult);
+    }
+
+    public function checkToken(Request $request)
+    {
+
+    }
+
+    private function createFirebaseAuth()
     {
         $factory = (new Factory)->withServiceAccount('../keys/firebase_credentials.json');
-        $auth = $factory->createAuth();
-
-        $email = 'admin@mc2.fr';
-        $password = 'password';
-
-        $signInResult = $auth->signInWithEmailAndPassword($email, $password);
-
-        dd($signInResult);
-
-        $uid = 'some-uid';
-
-        $customToken = $auth->createCustomToken($uid);
-
-        $userProperties = [
-            'email' => 'user@example.com',
-            'emailVerified' => false,
-            'phoneNumber' => '+15555550100',
-            'password' => 'secretPassword',
-            'displayName' => 'John Doe',
-            'photoUrl' => 'http://www.example.com/12345678/photo.png',
-            'disabled' => false,
-        ];
-
-        $createdUser = $auth->createUser($userProperties);
-        dd($createdUser);
-
-        return $this->json([
-                'user' => $this->getUser() ? $this->getUser()->getId() : null]
-        );
+        return $factory->createAuth();
     }
+
 }
