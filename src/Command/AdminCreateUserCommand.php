774b51ea71f089412f27a4499f6d23056e76eab8
gdarquie <gaetan.darquie@gmail.com>
2020-08-11 22:51:58 +0200

    Prepare logic for launching import

--- src/Command/AdminCreateUserCommand.php
+++ src/Command/AdminCreateUserCommand.php
@@ -5,9 +5,7 @@ namespace App\Command;
 use App\Component\DTO\Admin\CreateUser;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -16,11 +14,8 @@ class AdminCreateUserCommand extends Command
 {
     protected static $defaultName = 'admin:create';
 
-    /** @var EntityManagerInterface */
-    private $em;
-
-    /** @var UserPasswordEncoderInterface */
-    private $encoder;
+    private EntityManagerInterface $em;
+    private UserPasswordEncoderInterface $encoder;
 
     public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $encoder, string $name = null)
     {
@@ -38,7 +33,6 @@ class AdminCreateUserCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $password = 'from command';
         $io = new SymfonyStyle($input, $output);
         CreateUser::execute($this->em, $this->encoder);
         $io->success("User has been created if it hasn't already exist");
--- src/Component/DataPersister/ImportDataPersister.php
+++ src/Component/DataPersister/ImportDataPersister.php
@@ -4,9 +4,20 @@ namespace App\Component\DataPersister;
 
 use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 use App\Entity\Import;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ImportDataPersister implements ContextAwareDataPersisterInterface
 {
+    private HttpClientInterface $client;
+    private EntityManagerInterface $em;
+
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $em)
+    {
+        $this->client = $client;
+        $this->em = $em;
+    }
+
     public function supports($data, array $context = []): bool
     {
         return $data instanceof Import;
@@ -14,8 +25,16 @@ class ImportDataPersister implements ContextAwareDataPersisterInterface
 
     public function persist($data, array $context = [])
     {
-        // when persist launch
+        // get data and save the import
+        $this->em->persist($data);
+        $this->em->flush();
 
+        // launch the import by
+        $result = $this->client->request('POST', $_ENV['IMPORTER_API_URL'], [
+            //todo : add the key
+        ]);
+
+        dd($result);
         dd('last here after transformation and security for saving');
         //todo : launch import here
     }
