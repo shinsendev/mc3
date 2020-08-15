586dccd864e776ba1a06f134fb3ccbe846c2a0c6
gdarquie <gaetan.darquie@gmail.com>
2020-08-15 21:27:51 +0200

    Remove useless command

--- src/Component/Shell/indexation.sh
+++
@@ -1,9 +0,0 @@
-echo "Indexation shell script hase been launched"
-#sudo php bin/console stats:person:update
-echo "Stats have be generated"
-#sudo php bin/console elastic:populate
-echo "Elastic Search indexes have been updated"
-#sudo php bin/console algolia:populate
-echo "Algolia index has been updated"
-cd ../../../
-ls
