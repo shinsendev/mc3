38ef2f1c17e91fb50434a82d3416e6ccadaa2fb5
gdarquie <gaetan.darquie@gmail.com>
2020-08-26 00:30:56 +0200

    Complete export CSV ready to test with simple songs

--- src/Component/Hydrator/Strategy/Export/CsvNumberHydrator.php
+++ src/Component/Hydrator/Strategy/Export/CsvNumberHydrator.php
@@ -31,7 +31,32 @@ class CsvNumberHydrator implements HydratorStrategyInterface
         $dto->setDubbing($number->getDubbing());
         $dto->setUuid($number->getUuid());
 
+        $numberAttributes = $number->getAttributes();
         // number attributes
+        $dto->setBeginning(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'begin_thesaurus'));
+        $dto->setEnding(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'ending_thesaurus'));
+        $dto->setOutlines(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'complet_options'));
+        $dto->setCompleteness(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'completeness_thesaurus'));
+        $dto->setStructure(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'structure'));
+        $dto->setPerformance(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'performance_thesaurus'));
+        $dto->setCast(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'cast'));
+        $dto->setSpectators(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'spectators_thesaurus'));
+        $dto->setDiegeticPerformance(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'performance_thesaurus'));
+        $dto->setMusicians(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'musician_thesaurus'));
+        $dto->setTopic(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'genre'));
+        $dto->setDiegeticPlace(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'diegetic_place_thesaurus'));
+        $dto->setImaginaryPlace(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'imaginary'));
+        $dto->setStereotype(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'stereotype'));
+        $dto->setExoticism(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'exoticism_thesaurus'));
+        $dto->setMusicalEnsemble(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'musensemble'));
+        $dto->setTempo(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'tempo_thesaurus'));
+        $dto->setMusicalStyles(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'musical_thesaurus'));
+        $dto->setDanceEnsemble(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'dancemble'));
+        $dto->setDancingType(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'dancing_type'));
+        $dto->setDanceSubgenre(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'dance_subgenre'));
+        $dto->setDanceContent(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'dance_content'));
+        $dto->setSource(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'source_thesaurus'));
+        $dto->setQuotation(ExportCsvHydrator::getAttributesByCategoryCode($numberAttributes, 'quotation_thesaurus'));
 
         // number people
         $dto->setNumberDirectors(ExportCsvHydrator::getPeopleByProfession($em, ModelConstants::FILM_MODEL, $number->getUuid(), Person::DIRECTOR_PROFESSION));
@@ -39,7 +64,6 @@ class CsvNumberHydrator implements HydratorStrategyInterface
         $dto->setStars(ExportCsvHydrator::getPeopleByProfession($em, ModelConstants::FILM_MODEL, $number->getUuid(), Person::FIGURANT_PROFESSION));
         $dto->setArrangers(ExportCsvHydrator::getPeopleByProfession($em, ModelConstants::FILM_MODEL, $number->getUuid(), Person::ARRANGER_PROFESSION));
         $dto->setDanceDirector(ExportCsvHydrator::getPeopleByProfession($em, ModelConstants::FILM_MODEL, $number->getUuid(), Person::CHOREGRAPH_PROFESSION));
-
 
         return $dto;
     }
